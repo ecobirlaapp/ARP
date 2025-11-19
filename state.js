cfe6f9562565f5f3374d899e711f8c696894213e
ecobirlaapp <ecobirlaapp@gmail.com>
2025-11-19 14:48:03 +0530

    Create state.js

---
+++ state.js
@@ -0,0 +1,19 @@
+export let state = {
+    currentUser: null, 
+    userAuth: null,    
+    checkInReward: 10,
+    leaderboard: [],
+    departmentLeaderboard: [],
+    stores: [],
+    products: [],      
+    history: [],
+    dailyChallenges: [],
+    events: [],
+    userRewards: [],   
+    levels: [
+        { level: 1, title: 'Green Starter', minPoints: 0, nextMin: 1001 },
+        { level: 2, title: 'Eco Learner', minPoints: 1001, nextMin: 2001 },
+        { level: 3, title: 'Sustainability Leader', minPoints: 2001, nextMin: 4001 },
+    ],
+    currentUploadChallengeId: null 
+};
